bea916004508ed3f5cb851dce91d27f21b2374c3
jweboy <1259524459@qq.com>
2019-09-16 13:36:17 +0800

    :sparkles: Add EmptyData component

---
+++ lib/components/empty_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmptyData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('no data'),
+    );
+  }
+}
